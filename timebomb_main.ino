b9eda0df4894ce215b99d2c3aa955c37e61a48ab
colson15 <69868976+colson15@users.noreply.github.com>
2020-11-03 12:43:39 -0600

    Added audio game to the main file
    Returns 0 if successful, returns 1 if the player failed.

--- timebomb_main.ino
+++ timebomb_main.ino
@@ -1,15 +1,56 @@
-#include <Adafruit_CircuitPlayground.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_CircuitPlayground.h>
+#include <time.h>
 void setup() {
   // put your setup code here, to run once:
   CircuitPlayground.begin(); //Needed for the Circuit Playground library
+  srand(time(NULL));
 }
 int audioGame(){
   //This will contain the finished audio game
   //Int return used to determine success or failure
+    int i,j; //Counters
+  int test[8];
+  int guess[8];
+  int rand_num;
+  int life = 1;
+  int num;
+  for(i = 0; i < 8; i++){ //A loop to randomly play 8 high or low notes
+    num = rand() % 100;
+    if(num < 40){
+      test[i] = 0;
+      CircuitPlayground.playTone(294,100);
+    }else{
+      test[i] = 1;
+      CircuitPlayground.playTone(440,100);
+    }
+    delay(500);
+  }
+  for(i = 0; i < 8; i++){
+    while(!CircuitPlayground.leftButton() && !CircuitPlayground.rightButton()){
+      //J chillin' until you hit a button
+    }
+    if(CircuitPlayground.leftButton()){
+      guess[i] = 0;
+    }else if(CircuitPlayground.rightButton()){
+      guess[i] = 1;
+    }
+    delay(200);
+  }
+  for(i = 0; i < 8; i++){ //Checks the players guesses
+    if(test[i] != guess[i]){
+      life = 0; //If you are wrong, you forfeit your life privileges (The bomb goes off)
+    }
+  }
+  if(life == 0){
+    for(i = 0; i < 10; i++){
+      return 1;
+    }
+  }else if(life == 1){
+    for(i = 0; i < 10; i++){
+      return 0;
+    }    
+  }
 }
 void boom(){
   //This will be the code run when the player loses the game
