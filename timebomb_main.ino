de2456d3b9037ebcce3eb3fb831caf7f557c53df
colson15 <69868976+colson15@users.noreply.github.com>
2020-11-02 22:26:25 -0600

    Main file that will hold the final game

---
+++ timebomb_main.ino
@@ -0,0 +1,24 @@
+#include <Adafruit_CircuitPlayground.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <Adafruit_NeoPixel.h>
+void setup() {
+  // put your setup code here, to run once:
+  CircuitPlayground.begin(); //Needed for the Circuit Playground library
+}
+int audioGame(){
+  //This will contain the finished audio game
+  //Int return used to determine success or failure
+}
+void boom(){
+  //This will be the code run when the player loses the game
+}
+int wires(){
+  //This will contain the code for the wire game
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  //This will be the main game, use this for control flow of the game itself
+  //More functions will likely be needed, but this will give us a general start
+}
